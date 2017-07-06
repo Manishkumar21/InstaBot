25aed8797ebe65a715a80805defce755d7bd0621
Manish <khiladimanu1@gmail.com>
2017-07-06 18:33:55 +0530

    like_user_post.py file is modified

---
+++ like_user_post.py
@@ -0,0 +1,15 @@
+import requests
+from constant_variable import APP_ACCESS_TOKEN ,BASE_URL
+from get_user_post import get_user_post
+
+
+def like_user_post(insta_username):
+    media_id = get_user_post(insta_username)
+    print(media_id)
+    request_url = (BASE_URL + "media/"+media_id+"/likes")
+    payload = {"access_token" : APP_ACCESS_TOKEN}
+    post_a_like = requests.post(request_url,payload).json()
+    if post_a_like['meta']['code'] == 200:
+        print 'Like was successful!'
+    else:
+        print 'Your like was unsuccessful. Try again!'
