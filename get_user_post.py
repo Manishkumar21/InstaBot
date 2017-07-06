233aef068e03046d4cbc14f282298851c1b4b81b
manishkumar21 <khiladimanu1@gmail.com>
2017-07-06 17:36:11 +0530

    get_user_post.py file is modified

---
+++ get_user_post.py
@@ -0,0 +1,29 @@
+import requests
+import urllib
+from get_user_id import *
+from constant_variable import *
+
+def get_user_post(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            (urllib.urlretrieve(image_url, image_name))
+            print(user_media['data'][0]['id'])
+            print 'Your image has been downloaded!'
+            return user_media['data'][0]['id']
+
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+#get_user_post(insta_username="radhika12344")
