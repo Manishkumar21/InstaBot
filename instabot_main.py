3d3075a7ae7af45967da9013d6578597ae91d7f9
Manish <khiladimanu1@gmail.com>
2017-07-07 00:47:47 +0530

    instabot_main.py File is Modified

--- instabot_main.py
+++ instabot_main.py
@@ -5,10 +5,11 @@ from like_user_post import like_user_post
 from comment_user_post import comment_user_post1
 from self_info import self_info
 
-print("\t\t\t*****Welcome To InstaBot*****")
-menu_choices = "What do you want to do. Select from the below Choices (1-6) \n\t1. Show Own Profile  \n\t2. Download Own Post " \
-               "\n\t3. Get User's Information. \n\t4. Download User's Recent Post \n\t5. Like User's Post" \
-               " \n\t6. Comment on User's Post \n\t7. Close Application"
+print("\t\t\t*****Welcome To InstaBot*****\n")
+menu_choices = "What do you want to do. Select from the below Choices (1-6)\n\n\t\t1. Show Own Profile  \n\t\t2. Download Own Post " \
+               "\n\t\t3. Get User's Information. \n\t\t4. Download User's Recent Post \n\t\t5. Like User's Post" \
+               " \n\t\t6. Comment on User's Post \n\t\t7. Close Application  "
+
 show_menu = True
 while show_menu:
     menu_choice = input(menu_choices)
@@ -19,37 +20,33 @@ while show_menu:
             self_info()
             print("\n\n")
 
-
         elif menu_choice == 2:
             print (".....Downloading Your Own Recent Post.....\n")
             get_own_post()
             print("\n")
-            print("\n")
 
         elif menu_choice == 3:
-            insta_username = raw_input("Enter Username.........\n")
-            print("Wait getting information.....")
+            insta_username = raw_input("Enter The Username Who's Information You Want. \n")
+            print("Wait ** Information is Downloading...")
             get_user_info(insta_username)
-            print("\n")
             print("\n")
 
         elif menu_choice == 4:
-            insta_username = raw_input("Enter Username.........\n")
-            print("Wait Downloading user post......")
+            insta_username = raw_input("Enter The Username Who's Recent Post You Want To Download. \n")
+            print("Wait ** User's Recent Post is Downloading...")
             get_user_post(insta_username)
-
-            print("\n")
             print("\n")
 
         elif menu_choice == 5:
-            insta_username = raw_input("Enter Username")
-            print("Wait liking Ur POst......")
+            insta_username = raw_input("Enter The Username Who's Recent Post You Want To Like. ")
+            print("Wait ** Liking The User's Recent Post...")
             like_user_post(insta_username)
+            print("\n")
 
         elif menu_choice == 6:
-            insta_username = raw_input("Enter Username.........\n")
+            insta_username = raw_input("Enter The Username Who's Recent Post You Want To Write Something. \n")
+            print("Wait ** Commenting in the User's Recent Post...")
             comment_user_post1(insta_username)
-            print("\n")
             print("\n")
 
         elif menu_choice == 6:
