0cb8ab8e3a93f988b242180743d5ca3dcc9eb92f
manishkumar21 <khiladimanu1@gmail.com>
2017-07-06 17:23:43 +0530

    Constant_variable.py file is created

---
+++ constant_variable.py
@@ -0,0 +1,6 @@
+ #All global variable are declared here so we can call them from all using one file
+
+#instagram API acess token
+APP_ACCESS_TOKEN = "1579830763.54aecc6.574110acbe204361ae76d0379dd44f4f"
+#instagram base URL
+BASE_URL = "https://api.instagram.com/v1/"
