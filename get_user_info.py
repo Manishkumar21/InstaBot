13f033317c227249860bb165d8181693634a3115
manishkumar21 <khiladimanu1@gmail.com>
2017-07-06 17:30:59 +0530

    get_user_info.py file is modified

---
+++ get_user_info.py
@@ -0,0 +1,26 @@
+import requests
+from constants import *
+from get_user_id import *
+
+
+def get_user_info(insta_username):
+    #function logic here
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'There is no data for this user!'
+    else:
+        print 'Status code other than 200 received!'
+#get_user_info(insta_username="radhika12344")
