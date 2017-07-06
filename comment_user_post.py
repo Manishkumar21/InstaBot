eff0b8877bf55bfefe300850a59f7b14f4202da0
Manish <khiladimanu1@gmail.com>
2017-07-06 18:35:29 +0530

    comment_user_post.py file is modified

---
+++ comment_user_post.py
@@ -0,0 +1,19 @@
+import requests
+from constant_variable import APP_ACCESS_TOKEN ,BASE_URL
+from get_user_post import get_user_post
+
+
+def comment_user_post(insta_username):
+    media_id = get_user_post(insta_username)
+    print(media_id)
+    message = raw_input("Enter ur comment....\n")
+    payload = {"access_token" : APP_ACCESS_TOKEN, "message":message}
+    request_url = (BASE_URL + "media/" + media_id + "/comments")
+    post_a_comment = requests.post(request_url,payload).json()
+    print 'POST request url : %s' % (request_url)
+
+    print(post_a_comment['meta']['code'])
+    if post_a_comment['meta']['code'] == 200:
+        print("Post comment successfully")
+    else :
+        print('not successful')
