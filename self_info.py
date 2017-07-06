c3672ced6045acb4d1a73a0baa4237e92ffa7653
manishkumar21 <khiladimanu1@gmail.com>
2017-07-06 17:27:02 +0530

    Self_info.py file is modified

---
+++ self_info.py
@@ -0,0 +1,21 @@
+import requests
+from constant_variable import APP_ACCESS_TOKEN, BASE_URL
+
+
+def self_info():
+    #logic of the function
+    print("")
+    request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'User does not exist!'
+    else:
+        print 'Status code other than 200 received!'
