510292c4da93716b008917ac3e1f8f1ce34cc750
Manish <khiladimanu1@gmail.com>
2017-07-08 15:15:49 +0530

    get_user_post file Added and Updated

---
+++ get_post_id.py
@@ -0,0 +1,22 @@
+from get_user_id import get_user_id
+from constant_variable import BASE_URL,APP_ACCESS_TOKEN
+import requests
+
+def get_post_id(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            return user_media['data'][0]['id']
+        else:
+            print 'There is no recent post of the user!'
+            exit()
+    else:
+        print 'Status code other than 200 received!'
+        exit()
