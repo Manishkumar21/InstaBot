5e9d1d722ca449fe2943bc3f66fed85b81a2719c
Manish <khiladimanu1@gmail.com>
2017-07-08 15:24:53 +0530

    delete_negative_comment.py file Updated

--- delete_negative_comments.py
+++ delete_negative_comments.py
@@ -1,6 +1,8 @@
 import requests
 from constant_variable import APP_ACCESS_TOKEN ,BASE_URL
 from get_post_id import get_post_id
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
 
 def delete_negative_comment(insta_username):
     media_id = get_post_id(insta_username)
@@ -10,8 +12,24 @@ def delete_negative_comment(insta_username):
 
     if comment_info['meta']['code'] == 200:
         if len(comment_info['data']):
-            pass
+            for x in range(0, len(comment_info['data'])):
+                comment_id = comment_info['data'][x]['id']
+                comment_text = comment_info['data'][x]['text']
+                blob = TextBlob(comment_text, analyzer=NaiveBayesAnalyzer())
+                if (blob.sentiment.p_neg > blob.sentiment.p_pos):
+                    print 'Negative comment : %s' % (comment_text)
+                    delete_url = (BASE_URL + 'media/%s/comments/%s/?access_token=%s') % (
+                    media_id, comment_id, APP_ACCESS_TOKEN)
+                    print 'DELETE request url : %s' % (delete_url)
+                    delete_info = requests.delete(delete_url).json()
+
+                    if delete_info['meta']['code'] == 200:
+                        print 'Comment successfully deleted!\n'
+                    else:
+                        print 'Unable to delete comment!'
+                else:
+                    print 'Positive comment : %s\n' % (comment_text)
         else:
             print 'There are no existing comments on the post!'
     else:
-        print 'Status code other than 200 received!'
+         print 'Status code other than 200 received!'
